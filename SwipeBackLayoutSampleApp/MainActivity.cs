fe5d8466bee31dd76c309322ab2698681a00a9f5
Guillermo Tinoco <m3mo89@gmail.com>
2017-05-09 12:46:35 -0500

    Changed the minimum version, fixed errors and sample app

---
+++ SwipeBackLayoutSampleApp/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Android.SwipeBackLayout.App;
+
+
+namespace SwipeBackLayoutSampleApp
+{
+	[Activity(Label = "SwipeBackLayoutSampleApp", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : SwipeBackActivity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetTheme(Resource.Style.Theme_AppCompat_Light_NoActionBar);
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+		}
+	}
+}
+
